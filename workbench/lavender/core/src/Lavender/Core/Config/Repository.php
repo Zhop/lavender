4961df17ca80341dff03df4cea362fab5b4edc05
Daniel Sloof <goapsychadelic@gmail.com>
2014-05-04 16:56:17 +0200

    allow fetching of layout configurations

--- workbench/lavender/core/src/Lavender/Core/Config/Repository.php
+++ workbench/lavender/core/src/Lavender/Core/Config/Repository.php
@@ -8,14 +8,31 @@ class Repository extends \Illuminate\Config\Repository
     /**
      * Get the specified configuration value.
      *
+     * @todo   Cache certain configuration values.
      * @param  string  $key
      * @param  mixed   $default
      * @return mixed
      */
     public function get($key, $default = null)
     {
-        //do cache
         return parent::get($key, $default);
+    }
+
+    /**
+     * Gets layout configuration for all loaded packages.
+     *
+     * @return array
+     */
+    public function getLayoutConfiguration()
+    {
+        $configuration = array();
+        foreach ($this->packages as $namespace) {
+            $layoutConfig = $this->get($namespace . '::layout');
+            if ($layoutConfig) {
+                $configuration[] = $layoutConfig;
+            }
+        }
+        return $configuration;
     }
 
 }
